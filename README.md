5bb748991a91b6532d36769502aadc96e7ca436b
WilliamRClark <cwilliamwork@yahoo.com>
2016-11-25 11:48:31 -0500

    Update phaser game.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # phaser-starter
+
+![alt text](./src/assets/phaser.png "Phaser Icon")
+
+This is a starter kit for any Phaser game.  It contains the basics you will need to work with Phaser effectively.
+
+## Getting Started
+Yeah, the boring command line stuff.  
+
+To get started you'll first want to bring in Phaser, and the rest of the JavaScript dependant files for your project. This can be easily done from the command line using [NodeJS](https://nodejs.org/en/):
+```
+npm install
+```
+
+The next thing we want to do is to compile the source code.  There are several ways to do this.  
--- src/game.ts
+++ src/game.ts
@@ -1,21 +1,40 @@
 namespace Game {
+    
+    class Game {
+        private game: Phaser.Game;
+        private logoSprite: Phaser.Sprite;
 
-        var game : Phaser.Game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: preload, create: create });
+        constructor() {
+            this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
+        }
 
         /**
          * Load game assets.
          */
-        function preload () {
-            game.load.image('logo', 'assets/phaser.png');
+        preload() {
+            this.game.load.image('logo', 'assets/phaser.png');
         }
 
         /**
          * Initialize our display
          */
-        function create () {
-            var logo : Phaser.Sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-            logo.anchor.setTo(0.5, 0.5);
+        create() {
+            this.logoSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+            this.logoSprite.anchor.setTo(0.5, 0.5);
         }
 
+        /**
+         * Game loop
+         */
+        update() {
+            this.logoSprite.x += 1;
+            this.logoSprite.y += 1;
+        }
+
+    }
+
+    window.onload = () => {
+        var gameInstance = new Game();
+    }
 }
 
